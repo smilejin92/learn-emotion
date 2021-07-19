6859f7fe1d00b689b2ec6ad3c145ae2e28517368
smilejin92 <smilejin92@gmail.com>
2021-07-20 00:41:31 +0900

    [feat] EmotionComp 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { EmotionCompObj, EmotionCompString } from './components/Emotion';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EmotionCompObj>css prop - string style 사용</EmotionCompObj>
+      <EmotionCompString>css prop - object style 사용</EmotionCompString>
     </div>
   );
 }
