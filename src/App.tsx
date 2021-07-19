52d2996acc281392ee3e161dd3ee8ab6cf2754cd
smilejin92 <smilejin92@gmail.com>
2021-07-20 00:43:05 +0900

    [feat] StyleOrder 컴포넌트 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import { EmotionCompObj, EmotionCompString } from './components/Emotion';
+import { ArticleText, P, SmallArticleText } from './components/StyleOrder';
 
 function App() {
   return (
     <div className="App">
       <EmotionCompObj>css prop - string style 사용</EmotionCompObj>
       <EmotionCompString>css prop - object style 사용</EmotionCompString>
+      <P>normal text</P>
+      <ArticleText>article text</ArticleText>
+      <SmallArticleText>small article text</SmallArticleText>
     </div>
   );
 }
