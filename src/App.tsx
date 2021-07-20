dc93a2dbccdecc7738276544f5749c703960b172
smilejin92 <smilejin92@gmail.com>
2021-07-20 23:32:29 +0900

    [feat] Composition test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import './App.css';
 import { EmotionCompObj, EmotionCompString } from './components/Emotion';
 import {
@@ -9,6 +11,15 @@ import {
 import { ArticleText, P, SmallArticleText } from './components/StyleOrder';
 
 function App() {
+  const dangerStyle = css`
+    color: red;
+  `;
+
+  const baseStyle = css`
+    color: turquoise;
+    background-color: darkgreen;
+  `;
+
   return (
     <div className="App">
       <EmotionCompObj>css prop - string style 사용</EmotionCompObj>
@@ -20,6 +31,15 @@ function App() {
       <PrimaryButton>Primary Button</PrimaryButton>
       <DangerButton>Danger Button</DangerButton>
       <Child>Child</Child>
+      <div>
+        <div css={baseStyle}>color는 turquoise이다.</div>
+        <div css={[dangerStyle, baseStyle]}>
+          base 클래스가 더 나중에 전달되었기 때문에 color는 turquoise이다.
+        </div>
+        <div css={[baseStyle, dangerStyle]}>
+          danger 클래스가 더 나중에 전달되었기 때문에 color는 red이다.
+        </div>
+      </div>
     </div>
   );
 }
