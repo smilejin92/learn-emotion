2c4f2ae1238004a66692f041ef2a1b166a8a1ff4
smilejin92 <smilejin92@gmail.com>
2021-07-21 18:29:08 +0900

    [feat] ThemeProvider, keyframes 컴포넌트 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
+import { css, ThemeProvider } from '@emotion/react';
 import './App.css';
 import { EmotionCompObj, EmotionCompString } from './components/Emotion';
 import { GlobalStyles } from './components/GlobalStyles';
+import { BouncingText } from './components/Keyframes';
 import { NestedStyle } from './components/NestedSelector';
 import { ResponsiveComp1, ResponsiveComp2 } from './components/ResponsiveComps';
 import {
@@ -12,6 +13,7 @@ import {
   PrimaryButton,
 } from './components/StyledComponents';
 import { ArticleText, P, SmallArticleText } from './components/StyleOrder';
+import { ThemedComp1, ThemedComp2, ThemedComp3 } from './components/ThemedComp';
 
 function App() {
   const dangerStyle = css`
@@ -23,32 +25,44 @@ function App() {
     background-color: darkgreen;
   `;
 
+  const theme = {
+    colors: {
+      primary: 'blue',
+    },
+  };
+
   return (
     <>
       <GlobalStyles />
-      <div className="App">
-        <EmotionCompObj>css prop - string style 사용</EmotionCompObj>
-        <EmotionCompString>css prop - object style 사용</EmotionCompString>
-        <P>normal text</P>
-        <ArticleText>article text</ArticleText>
-        <SmallArticleText>small article text</SmallArticleText>
-        <Button>Styled Button</Button>
-        <PrimaryButton>Primary Button</PrimaryButton>
-        <DangerButton>Danger Button</DangerButton>
-        <Child>Child</Child>
-        <div>
-          <div css={baseStyle}>color는 turquoise이다.</div>
-          <div css={[dangerStyle, baseStyle]}>
-            base 클래스가 더 나중에 전달되었기 때문에 color는 turquoise이다.
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <EmotionCompObj>css prop - string style 사용</EmotionCompObj>
+          <EmotionCompString>css prop - object style 사용</EmotionCompString>
+          <P>normal text</P>
+          <ArticleText>article text</ArticleText>
+          <SmallArticleText>small article text</SmallArticleText>
+          <Button>Styled Button</Button>
+          <PrimaryButton>Primary Button</PrimaryButton>
+          <DangerButton>Danger Button</DangerButton>
+          <Child>Child</Child>
+          <div>
+            <div css={baseStyle}>color는 turquoise이다.</div>
+            <div css={[dangerStyle, baseStyle]}>
+              base 클래스가 더 나중에 전달되었기 때문에 color는 turquoise이다.
+            </div>
+            <div css={[baseStyle, dangerStyle]}>
+              danger 클래스가 더 나중에 전달되었기 때문에 color는 red이다.
+            </div>
           </div>
-          <div css={[baseStyle, dangerStyle]}>
-            danger 클래스가 더 나중에 전달되었기 때문에 color는 red이다.
-          </div>
+          <NestedStyle />
+          <ResponsiveComp1 />
+          <ResponsiveComp2 />
+          <BouncingText />
+          <ThemedComp1 />
+          <ThemedComp2>Themed Component 2</ThemedComp2>
+          <ThemedComp3 />
         </div>
-        <NestedStyle />
-        <ResponsiveComp1 />
-        <ResponsiveComp2 />
-      </div>
+      </ThemeProvider>
     </>
   );
 }
